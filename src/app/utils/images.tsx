6a5e1272a69c642bd3bd5b5e59b58a7ad086de42
yasovardhan masani <yasovardhanmasani@gmail.com>
2024-05-10 09:23:22 +0530

    initial commit after kinde auth and landing page

---
+++ src/app/utils/images.tsx
@@ -0,0 +1,28 @@
+import laughingSnake from '../../../public/snake-1.png';
+import crawlingSnake from '../../../public/snake-2.png'
+import snakeInBox from "../../../public/snake-3.png"
+import horsePicture from "../../../public/horse.jpg"
+import horsePhonePicture from "../../../public/horse_phone.jpg"
+import clearPhone from "../../../public/clearphone.png"
+import darkPhoneTemplate from "../../../public/phone-template-dark-edges.png"
+import lightPhoneTemplate from "../../../public/phone-template-white-edges.png"
+import thumbnail from "../../../public/thumbnail.png"
+import yourImage from "../../../public/your-image.png"
+import arrow from "../../../public/arrow.png"
+import phoneTemplate from "../../../public/phone-template.png"
+import PeopleBuying from "../../../public/what-people-are-buying.png"
+import FirstUser from "../../../public/users/user-1.png"
+import SecondUser from "../../../public/users/user-2.png"
+import ThirdUser from "../../../public/users/user-3.png"
+import FourthUser from "../../../public/users/user-4.jpg"
+import FifthUser from "../../../public/users/user-5.jpg"
+import FirstTestimonial from "../../../public/testimonials/1.jpg"
+import SecondTestimonial from "../../../public/testimonials/2.jpg"
+import ThirdTestimonial from "../../../public/testimonials/3.jpg"
+import FourthTestimonial from "../../../public/testimonials/4.jpg"
+import FifthTestimonial from "../../../public/testimonials/5.jpg"
+import SixthTestimonial from "../../../public/testimonials/6.jpg"
+import line from "../../../public/line.png"
+
+ 
+export {laughingSnake,crawlingSnake,snakeInBox,horsePicture,horsePhonePicture,clearPhone,darkPhoneTemplate,lightPhoneTemplate,thumbnail,yourImage,arrow,phoneTemplate,PeopleBuying,FirstUser,SecondUser,ThirdUser,FourthUser,FifthUser,FirstTestimonial,SecondTestimonial,ThirdTestimonial,FourthTestimonial,FifthTestimonial,SixthTestimonial,line}
